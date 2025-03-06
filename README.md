b6635f2eba9db7c2c56390655bf41ba61e075bd9
Leevon Levasseur <leevonlevasseur@gmail.com>
2025-03-06 14:25:27 -0800

    Initial commit and vite scaffold

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gemini AI 101 🤖
+
+A simple promting app that returns an Emoji based on the input text.
+
+## Backend GitHub Repo
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Gemini 101 🤖</h1>
+    </>
+  );
+}
+
+export default App;
