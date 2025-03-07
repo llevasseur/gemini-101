9210a60d88164a842c996d9b8e1ff6b11dd0e405
Leevon Levasseur <leevonlevasseur@gmail.com>
2025-03-06 16:00:21 -0800

    adds About page and template styling for future pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./App.scss";
 
 import Header from "./components/Header/Header";
 import GenPage from "./pages/GenPage/GenPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/generateEmoji" />} />
-        <Route path="/generateEmoji" element={<GenPage />} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/*" element={<h1>404 Not Found</h1>} />
-      </Routes>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Navigate to="/generateEmoji" />} />
+          <Route path="/generateEmoji" element={<GenPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
