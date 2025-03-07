1e006437c50d7878a3589267410fb7663b88f0a7
Leevon Levasseur <leevonlevasseur@gmail.com>
2025-03-06 17:11:12 -0800

    adds toast and error handling to form

--- src/components/GenForm/GenForm.jsx
+++ src/components/GenForm/GenForm.jsx
@@ -1,27 +1,52 @@
 import { useState } from "react";
 import "./GenForm.scss";
-function GenForm() {
+function GenForm({ generateEmoji }) {
   const [textareaInput, setTextareaInput] = useState("");
+  const [errorText, setErrorText] = useState("");
   function handleTextareaChange(event) {
     setTextareaInput(event.target.value);
   }
+
+  function isFormValid() {
+    if (!textareaInput.trim()) {
+      setErrorText("Please enter a prompt");
+      return false;
+    }
+    return true;
+  }
+
   function handleClear(event) {
     event.preventDefault();
     setTextareaInput("");
   }
   function handleSubmit(event) {
     event.preventDefault();
-    alert("submitted");
-    setTextareaInput("");
+    if (isFormValid()) {
+      generateEmoji("test");
+      setTextareaInput("");
+    }
   }
   return (
     <form className="gen-form">
+      <div className="error-block">
+        {errorText ? (
+          <p className="error-block__text">🛑 {errorText}</p>
+        ) : (
+          <p className="error-block__text"></p>
+        )}
+      </div>
       <textarea
-        className="gen-form__textarea"
+        className={
+          errorText
+            ? "gen-form__textarea gen-form__textarea--error"
+            : "gen-form__textarea"
+        }
         onChange={handleTextareaChange}
+        onFocus={() => setErrorText("")}
         value={textareaInput}
         placeholder="Type something and generate an emoji 😜"
       ></textarea>
+
       <button onClick={handleClear}>Clear</button>
       <button type="submit" onClick={handleSubmit}>
         Generate
